2e257023c6a41d54e9cd097d920ed071e411cfe2
mamedinas1 <miaferrera@me.com>
2020-05-22 15:56:44 -0400

    IT LIVESSSSS

--- miaCreateMeetingComp/force-app/main/default/aura/CreateMeeting/CreateMeetingController.js
+++ miaCreateMeetingComp/force-app/main/default/aura/CreateMeeting/CreateMeetingController.js
@@ -1,6 +1,10 @@
 ({
+    //upon init, use the getAll Apex method
     doInit : function(component, event, helper) {
         var action = component.get("c.getAll");
+        //set yer callback. in this case, we unpack the
+        //result (objs from Apex getAll method)
+        //to populate the picklists for the user.
         action.setCallback(this, function(response){
             var state = response.getState();
             if(state === "SUCCESS")
@@ -16,28 +20,67 @@
     },
 
     clickCreate : function(component, event, helper) {
+        //get all the things from the view
         var meet = component.get("v.meeting");
         var entireBatch = component.get("v.allbatch");
         var batch = component.get("v.selBatch");
-        var interviewer = content.get("v.selInterviewer");
+        var interviewer = component.get("v.selInterviewer");
         var associate = null;
-        
+
+        //if it's not an entire batch selected, get the selected associate
         if(!entireBatch)
             associate = component.get("v.selAssociate");
+        
+        //check for issues
+        console.log(batch + " " + interviewer + " " + associate);
+        //call the createMeeting Apex method
+        var action = null;
 
-        var action = component.get("c.createMeeting");
+        if(entireBatch)
+        {
+            action = component.get("c.createMeetingAllBatch");
+            action.setParams({
+                meeting : meet,
+                strBatch : batch,
+                strInterviewer : interviewer,
+                fullBatch : component.get("v.ascbatchmap")[batch]
+            });
+        }
+        else
+        {
+            action = component.get("c.createMeeting");
+            action.setParams({
+                meeting : meet,
+                strBatch : batch,
+                strInterviewer : interviewer,
+                strAssociate : associate
+            });
+        }
+            
+        //pass these to the createMeeting method
         
-        action.setParams({"meeting":meet,"batch":batch,"interviewer":interviewer,"associate":associate});
-        
+        //set yer callback
         action.setCallback(this,function(response){
             var state = response.getState();
+           
+            //check for issues
+            console.log(state);
+            
             if(state === "SUCCESS")
             {
                 var meeting = response.getReturnValue();
+                component.set("v.meeting", {"sObject":"Meeting__c"});
+                component.set("v.selInterviewer", "");
+                component.set("v.selBatch", "");
+                component.set("v.selAssociate", "");
             }
+            else if (state === "ERROR")
+            {
+                response.get
+            }
+            //reset form
+            //show success message if meeting is created
         });
-
-        
         $A.enqueueAction(action);
     },
 
@@ -51,7 +94,7 @@
 
     //whatever component wants this event needs to handle it, probably the meeting tiles or meeting assignment
     throwTheMeeting : function(component, event, helper) {
-            var throwMeeting = cmp.getEvent("throwMeeting");
+            var throwMeeting = component.getEvent("throwMeeting");
             //throwMeeting.setParams
             //throwMeeting.fire();
     }
