79112b4881d5600901af78fd58c21c92c8c42818
Nicholas Breazeale <breazn95@gmail.com>
2020-05-22 10:09:31 -0700

    Added functionality to the completed interviews component.

--- force-app/main/default/aura/completed_interviews/completed_interviewsController.js
+++ force-app/main/default/aura/completed_interviews/completed_interviewsController.js
@@ -1,5 +1,29 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+	init: function(component, event, helper) {
+		component.set("v.columns", [
+			{
+				label: "Meeting Name",
+				fieldName: "Name",
+				type: "String"
+			},
+			/*
+			{
+				label: "Associate",
+				fieldName: "",
+				type: ""
+			},
+			{
+				label: "Interviewer",
+				fieldName: "",
+				type: ""
+			},
+			*/
+			{
+				label: "Completion Date",
+				fieldName: "Completed_Date__c",
+				type: "date"
+			}
+		]);
+		helper.getMeetings(component);
+	}
 })
--- force-app/main/default/aura/completed_interviews/completed_interviewsHelper.js
+++ force-app/main/default/aura/completed_interviews/completed_interviewsHelper.js
@@ -1,5 +1,13 @@
 ({
-    helperMethod : function() {
-
-    }
+    getMeetings: function(component) {
+		var action = component.get("c.getMeetings");
+		action.setCallback(this, $A.getCallback(function (response) {
+			if (response.getState() === "SUCCESS") {
+				component.set("v.meetings", response.getReturnValue());
+			} else {
+				console.error(response.getError());
+			}
+		}));
+		$A.enqueueAction(action);
+	}
 })
