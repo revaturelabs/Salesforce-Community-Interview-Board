85ecdb918201bccdf9f366afb9416f2b159bbafc
[mohmmeddrras] <[mohmmeddrras@hotmail.com]>
2020-05-26 07:09:16 -0700

    1

--- force-app/main/default/aura/MeetingTiles/MeetingTilesHelper.js
+++ force-app/main/default/aura/MeetingTiles/MeetingTilesHelper.js
@@ -1,5 +1,17 @@
 ({
-    helperMethod : function() {
-
+    getMeetings : function(component) {
+        var action = component.get("c.getMeetings");
+        
+        //Set up the callback
+        var self = this;
+        action.setCallback(this, function(actionResult) {
+            var state = actionResult.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                component.set("v.contacts", actionResult.getReturnValue());
+            }            
+        });
+        $A.enqueueAction(action);
     }
 })
+
+
