81d2d89baa000254366b9e8c76d86a6800ae86b8
[mohmmeddrras] <[mohmmeddrras@hotmail.com]>
2020-05-28 12:17:03 -0700

    updates

--- force-app/main/default/aura/BatchClientlist/BatchClientlistHelper.js
+++ force-app/main/default/aura/BatchClientlist/BatchClientlistHelper.js
@@ -1,5 +1,27 @@
 ({
-    helperMethod : function() {
-
+    getBatchClient: function(component, page, recordToDisply) {
+  
+       // create a server side action. 
+       var action = component.get("c.fetchBatchClient");
+       // set the parameters to method 
+       action.setParams({
+          "pageNumber": page,
+          "recordToDisply": recordToDisply
+       });
+       // set a call back   
+       action.setCallback(this, function(a) {
+          // store the response return value (wrapper class insatance)  
+          var result = a.getReturnValue();
+          console.log('result ---->' + JSON.stringify(result));
+          // set the component attributes value with wrapper class properties.   
+  
+          component.set("v.BatchClient", result.BatchClient);
+          component.set("v.page", result.page);
+          component.set("v.total", result.total);
+          component.set("v.pages", Math.ceil(result.total / recordToDisply));
+  
+       });
+       // enqueue the action 
+       $A.enqueueAction(action);
     }
-})
+ })
