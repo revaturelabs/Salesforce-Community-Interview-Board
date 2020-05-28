1b247ac2cd1b7a21d71b225802760c9e14b33e51
ejefferson88@yahoo.com <ejefferson88@yahoo.com>
2020-05-28 14:22:59 -0400

    More changes to class and cmp files

--- force-app/main/default/aura/InterviewQuestions/InterviewQuestionsController.js
+++ force-app/main/default/aura/InterviewQuestions/InterviewQuestionsController.js
@@ -3,26 +3,24 @@
         
         var mQuestions = component.get("v.questionMap");
         var action = component.get("c.loadQuestionMap");
-        var action2 = component.get("c.getMeetings");
+        var meetings = component.get("v.meetings");
+        
         action.setCallback(this,function(response){
             if(response.getState()=="SUCCESS")
             {
                 component.set("v.QuestionMap",response.getReturnValue());
+
+                for (let key of mQuestions.keys())
+                {
+                    meetings.push(key);
+                }
+
+                component.set("v.meetings",meetings);
             }
             
         });
         
         $A.enqueueAction(action);
-        
-        action2.setCallback(this,function(response){
-            if(response.getState()=="SUCCESS")
-            {
-                component.set("v.meetings",response.getReturnValue());
-            }
-            
-        });
-        
-        $A.enqueueAction(action2);
 		
 	}
 })
