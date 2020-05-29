e966029d0223cde1ac8179b2c165a540fe7e9b68
Nicholas Breazeale <breazn95@gmail.com>
2020-05-29 13:56:39 -0700

    Abandoned AssociateInterview branch, created new one.

--- force-app/main/default/aura/associateInterviews/associateInterviewsController.js
+++ force-app/main/default/aura/associateInterviews/associateInterviewsController.js
@@ -1,4 +1,5 @@
 ({
+	// Setup datatable columns
 	init: function(component, event, helper) {
 		component.set("v.columns", [
 			{
@@ -6,24 +7,63 @@
 				fieldName: "Name",
 				type: "String"
 			},
-			/*
 			{
-				label: "Associate",
-				fieldName: "",
-				type: ""
+				label: "Interviewer(s)",
+				fieldName: "Interviewers",
+				type: "String"
 			},
 			{
-				label: "Interviewer",
-				fieldName: "",
-				type: ""
-			},
-			*/
-			{
-				label: "Completion Date",
-				fieldName: "Completed_Date__c",
-				type: "date"
+				label: "Date",
+				fieldName: "Schedule",
+				type: "Datetime"
 			}
 		]);
-		helper.getMeetings(component);
+	},
+
+	search: function(component, event, helper) {
+		let searchInput = component.find("searchInput");
+
+		// Users must fix errors, other than custom ones, before searching
+		if (!searchInput.get("v.validity").valid && !searchInput.get("v.validity").customError) {
+			return;
+		}
+
+		// Clear custom error status
+		searchInput.setCustomValidity("");
+		searchInput.reportValidity();
+		// Indicate the search bar is loading
+		component.set("v.searchLoading", true);
+
+		// Initialize new action
+		var action = component.get("c.associateSearch");
+		action.setParams({associateId: component.get("v.associateId")});
+
+		action.setCallback(this, $A.getCallback(function (response) {
+			// Done loading, remove indicator
+			component.set("v.searchLoading", false);
+			
+			switch (response.getState()) {
+				case "SUCCESS":
+					// Populate table with values
+					component.set("v.meetings", response.getReturnValue());
+					break;
+				case "ERROR":
+					// Clear table on a server error
+					component.set("v.meetings", null);
+					// Display error message on search bar
+					let errors = response.getError();
+					if (errors) {
+						if (errors[0] && errors[0].message) {
+							searchInput.setCustomValidity(errors[0].message);
+						}
+					} else {
+						searchInput.setCustomValidity("Unknown error");
+					}
+					searchInput.reportValidity();
+					break;
+			}
+		}));
+
+		$A.enqueueAction(action);
 	}
 })
