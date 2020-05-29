fb4c7a738f196432b56e6368f824ad43c1a9739d
AsewAsew <dylanburk97@gmail.com>
2020-05-29 14:34:00 -0500

    Finished Interview Question Component

--- force-app/main/default/aura/InterviewQuestions/InterviewQuestionsController.js
+++ force-app/main/default/aura/InterviewQuestions/InterviewQuestionsController.js
@@ -1,11 +1,13 @@
 ({
 	doInit : function(component, event, helper) {
-        
+        //on init, get all the meetings and questions we need
         var action = component.get("c.loadQuestionMap");
         
         action.setCallback(this,function(response){
             if(response.getState()==="SUCCESS")
             {
+                //unpack the meetings from the List<List<Sobjects>>
+                //use the helper to unpack the questions
                 var result = response.getReturnValue();
                 component.set("v.meetings", result[0]);
                 helper.createMap(component, result[1]);
@@ -16,14 +18,23 @@
         $A.enqueueAction(action);
 		
     },
-    
+    //changing questions based on the meeting
     changeMeeting : function(component, event, helper) {
-        console.log("this");
         component.set("v.questions", component.get("v.questionMap")[event.getSource().get("v.value")]);
-        console.log("that");
+        helper.resetPage(component);
+        helper.disable(component);
     },
-
-    changeSubject : function(component, event, helper) {
-
-    }
+    
+    //pagination management
+    prevPage : function(component, event, helper) {
+        component.set("v.page", component.get("v.page") - 1);
+        helper.setPage(component);
+        helper.disable(component);
+    },
+    
+    nextPage : function(component, event, helper) {
+        component.set("v.page", component.get("v.page") + 1);
+        helper.setPage(component);
+        helper.disable(component);
+    },
 })
--- force-app/main/default/aura/InterviewQuestions/InterviewQuestionsHelper.js
+++ force-app/main/default/aura/InterviewQuestions/InterviewQuestionsHelper.js
@@ -1,6 +1,8 @@
 ({
     createMap : function(component, questions)
     {
+        //take the list of questions (result1), put it in a map
+        //Meeting ID is the key, question sObject is the value
         var qMap = component.get("v.questionMap");
         for(var i = 0; i < questions.length; i++)
         {
@@ -8,8 +10,59 @@
                 qMap[questions[i].Meeting__c] = [];
             qMap[questions[i].Meeting__c].push(questions[i]);
         }
-
+        //set the qMap to questionMap on the component
         component.set("v.questionMap", qMap);
-        //near, far, wherever you are
+        
+    },
+
+    //Handles disabling the "next" and "previous" buttons
+    //if we reach the beginning/end of pagination
+    disable : function(component)
+    {
+        var page = component.get("v.page");
+        var perPage = component.get("v.perPage");
+        var qCnt = component.get("v.questions").length;
+
+        if(page < 1)
+        {
+            page = 1;
+            component.set("v.page", 1);
+        }
+
+        component.set("v.disPrev", (page == 1));
+        component.set("v.disNext", (page * perPage >= qCnt));
+    },
+
+    //This breaks up the amount of questions per page
+    setPage : function(component)
+    {
+        var questions = component.get("v.questions");
+        var qSub = [];
+        var page = component.get("v.page") - 1;
+        var perPage = component.get("v.perPage");
+
+        for(var i = 0; i < perPage && i + page * perPage < questions.length; i++)
+        {
+            qSub.push(questions[i + page * perPage]);
+        }
+
+        component.set("v.qSub", qSub);
+    },
+
+    //if you change the meeting you pick, it should go back to page 1
+    resetPage : function(component)
+    {
+        component.set("v.page", 1);
+        this.setPage(component);
     }
 })
+
+/* if the createMap code was in Apex, this is what it'd be:
+Map<Id, List<sObject>> qMap = new Map<Id, List<sObject>>();
+for(Interview_Question__c question : questions)
+{
+    if(qMap.get(question.Meeting__c) == null)
+        qMap.put(question.Meeting__c, new List<sObject>());
+    qMap.get(question.Meeting__c).add(question);
+}
+*/
