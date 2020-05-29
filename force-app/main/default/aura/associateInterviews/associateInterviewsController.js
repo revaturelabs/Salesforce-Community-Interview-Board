1bc5e2dca577c19c391f26c0ea8c25f4a60181d9
Nicholas Breazeale <breazn95@gmail.com>
2020-05-29 11:59:28 -0700

    Search bar displays custom error messages.

--- force-app/main/default/aura/associateInterviews/associateInterviewsController.js
+++ force-app/main/default/aura/associateInterviews/associateInterviewsController.js
@@ -1,4 +1,5 @@
 ({
+	// Setup datatable columns
 	init: function(component, event, helper) {
 		component.set("v.columns", [
 			{
@@ -20,20 +21,49 @@
 	},
 
 	search: function(component, event, helper) {
-		let searchCmp = component.find("associateId");
+		let searchInput = component.find("searchInput");
+
+		// Users must fix errors, other than custom ones, before searching
+		if (!searchInput.get("v.validity").valid && !searchInput.get("v.validity").customError) {
+			return;
+		}
+
+		// Clear custom error status
+		searchInput.setCustomValidity("");
+		searchInput.reportValidity();
+		// Indicate the search bar is loading
 		component.set("v.searchLoading", true);
 
+		// Initialize new action
 		var action = component.get("c.associateSearch");
 		action.setParams({associateId: component.get("v.associateId")});
+
 		action.setCallback(this, $A.getCallback(function (response) {
+			// Done loading, remove indicator
 			component.set("v.searchLoading", false);
-			if (response.getState() === "SUCCESS") {
-				component.set("v.meetings", response.getReturnValue());
-				console.log(response.getReturnValue());
-			} else {
-				console.error(response.getError());
+			
+			switch (response.getState()) {
+				case "SUCCESS":
+					// Populate table with values
+					component.set("v.meetings", response.getReturnValue());
+					break;
+				case "ERROR":
+					// Clear table on a server error
+					component.set("v.meetings", null);
+					// Display error message on search bar
+					let errors = response.getError();
+					if (errors) {
+						if (errors[0] && errors[0].message) {
+							searchInput.setCustomValidity(errors[0].message);
+						}
+					} else {
+						searchInput.setCustomValidity("Unknown error");
+					}
+					searchInput.reportValidity();
+					break;
 			}
 		}));
+
 		$A.enqueueAction(action);
 	}
 })
