17097bac3958686eb4d80ea6f394f023183f4b3a
jacobwarshawer <31668596+jrwarshawer@users.noreply.github.com>
2020-06-01 13:47:30 -0700

    Final Commit on QuestionCreation

--- force-app/main/default/aura/QuestionCreation/QuestionCreationController.js
+++ force-app/main/default/aura/QuestionCreation/QuestionCreationController.js
@@ -2,8 +2,6 @@
     //Initial population of Meetings. Passes to a helper functions.
     init : function(component, event, helper) {
         helper.loadMeetings(component);
-        
-
     },
     
     //Create an InterviewQuestion record from user's input: subject and body
@@ -11,6 +9,7 @@
         //Retrieve the subject and question that was submitted by the user
         var sub = component.get("v.subject");
         var quest = component.get("v.question");
+        var meet = component.get("v.selectedMeetingId");
         
         //Subject is null
         if(sub == null){
@@ -18,13 +17,13 @@
         //Question is null
         } else if(quest == null) {
             alert("Please enter in values in body");
-        //Save to Database
+        //Save to database
         } else{
-            helper.saveQuestionHelper(component, sub, quest);
+            helper.saveQuestionHelper(component, sub, quest, meet);
         } 
     },
 
-    //Set selectedBatchId attribute with the Id of the batch account the user just selected from the dropdown menu
+    //Set selectedMeetingId attribute with the Id of the meeting the user selected from the dropdown menu
     meetingChange : function (component, event, helper) {
         component.set("v.selectedMeetingId",component.find("meetingPicker").get("v.value"));
     }
