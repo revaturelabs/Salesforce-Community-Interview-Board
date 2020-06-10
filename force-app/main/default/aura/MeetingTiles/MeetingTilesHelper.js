8a1da3f02d123b55f587d54d437acf180711a367
Lawrence <l.rush7@gmail.com>
2020-06-10 07:18:16 -0700

    pulled static resource

--- force-app/main/default/aura/MeetingTiles/MeetingTilesHelper.js
+++ force-app/main/default/aura/MeetingTiles/MeetingTilesHelper.js
@@ -2,7 +2,7 @@
   updateMeetings : function(component) {
     var action = component.get("c.updateMeetings");
     var meeting = component.get("v.Meeting"); 
-    action.setParams({UpdateMeeting:meeting}); 
+    action.setParams({updateMeeting:meeting}); 
     $A.enqueueAction(action);
   }
 })
