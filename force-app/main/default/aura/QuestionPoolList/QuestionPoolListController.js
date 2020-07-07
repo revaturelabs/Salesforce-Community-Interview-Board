6055f14df02c78b732a0f7b80672e7fb7df2ba8f
jhooper9 <j.hooper@gatech.edu>
2020-07-07 16:26:23 -0400

    Added the ability to delete one or more questions in the component

--- force-app/main/default/aura/QuestionPoolList/QuestionPoolListController.js
+++ force-app/main/default/aura/QuestionPoolList/QuestionPoolListController.js
@@ -19,6 +19,30 @@
             }
         });
         $A.enqueueAction(action);
-    }
+    },
+    select : function(component,event,helper) {
+        let qList = event.getParam("selectedRows");
+        component.set("v.selectedQuestions",qList);
+        console.log(qList);
+    },
+    
+    
+    deleteButton : function(component,event,helper) {
+        //let selectedRows =event.getParam("selectedRows");
+        let selectedRows = component.get("v.selectedQuestions");
+        let idList = [];
+        console.log(selectedRows);
+        for(let i=0;i<selectedRows.length;i++){
+            idList.push(selectedRows[i].Id);
+        }
+        let action = component.get("c.deleteQuestions");
+        action.setParams({"idList" : idList});
+    	action.setCallback(this,function(response){
+    		if(response.getState()==="SUCCESS"){
+                $A.get('e.force:refreshView').fire();
+			}
+ 		});
+    $A.enqueueAction(action);
+	}
+    
 })
-
