5d0ff596da9a9b060ddadd11551e71d68b24c029
jhooper9 <j.hooper@gatech.edu>
2020-07-10 14:32:45 -0400

    Transfered the role of interview question to question  in Question creation component

--- force-app/main/default/aura/QuestionCreation/QuestionCreationController.js
+++ force-app/main/default/aura/QuestionCreation/QuestionCreationController.js
@@ -1,7 +1,7 @@
 ({
     //Initial population of Meetings. Passes to a helper functions.
     init : function(component, event, helper) {
-        helper.loadMeetings(component);
+        helper.loadMeetingsAndTypeMap(component);
     },
     
     //Create an InterviewQuestion record from user's input: subject and body
@@ -10,6 +10,8 @@
         var sub = component.get("v.subject");
         var quest = component.get("v.question");
         var meet = component.get("v.selectedMeetingId");
+        var type = component.get("v.questionType");
+        var stack = component.get("v.questionStack");
         
         //Subject is null
         if(sub == null){
@@ -17,16 +19,36 @@
         //Question is null
         } else if(quest == null) {
             alert("Please enter in values in body");
+        //Question is null
+        } else if(type == null) {
+            alert("Please select a value for type");
+        //Question is null
+        } else if(stack == null) {
+            alert("Please select a value for stack");
         //Save to database
         } else{
-            helper.saveQuestionHelper(component, sub, quest, meet);
+            helper.saveQuestionHelper(component, sub, quest, type, stack, meet);
         } 
     },
 
     //Set selectedMeetingId attribute with the Id of the meeting the user selected from the dropdown menu
     meetingChange : function (component, event, helper) {
         component.set("v.selectedMeetingId",component.find("meetingPicker").get("v.value"));
-    }
-             
+    },
+    
+    changeTypes : function (component,event,helper) {
+        component.set("v.DisplayList", true);
+        var selectedStack = component.get("v.questionStack");
+        var stackTypeMap = component.get("v.typeMap");
+        var typeList = [];
+        var tempList = [];
+        tempList = stackTypeMap[selectedStack];
+        if (selectedStack) {
+            for(let type of tempList) typeList.push(type);
+                 
+            component.set("v.availableTypes", typeList);
+        }
+        component.set("v.DisplayList", true);
+    }         
             
 })
