444ec409389c0d4b4efaf3ef5965f87e7b6204d4
Andrew Miller <ajamesmiller35@gmail.com>
2020-07-16 14:04:55 -0600

    Fixed Synchronization

--- force-app/main/default/aura/ManualSync/ManualSyncController.js
+++ force-app/main/default/aura/ManualSync/ManualSyncController.js
@@ -7,20 +7,27 @@
         let updateNow = component.get('c.updateFromAura');
         updateNow.setCallback(this, function(response){
             let updateCounts = response.getReturnValue();
-            console.log(updateCounts);
             
-            let endTime = new Date();
-        	endTime = endTime.getTime();
+            if(updateCounts[0] == '-1' || updateCounts[1] == '-1'){
+                component.set('v.isLoading', false);
+                component.set('v.isError', true);
+            }
+            else{
+                let endTime = new Date();
+                endTime = endTime.getTime();
+                
+                let totalTime = endTime - startTime;
+                totalTime = totalTime/1000;
+                console.log(totalTime);
+                
+                component.set('v.batches', String(updateCounts[0]));
+                component.set('v.associates', String(updateCounts[1]));
+                component.set('v.time', totalTime);
+                component.set('v.isLoading', false);
+                component.set('v.isSuccess', true);
+            }
             
-            let totalTime = endTime - startTime;
-          	totalTime = totalTime/1000;
-            console.log(totalTime);
             
-            component.set('v.batches', String(updateCounts[0]));
-            component.set('v.associates', String(updateCounts[1]));
-            component.set('v.time', totalTime);
-            component.set('v.isLoading', false);
-            component.set('v.isSuccess', true);
         });
         $A.enqueueAction(updateNow);
 	}
