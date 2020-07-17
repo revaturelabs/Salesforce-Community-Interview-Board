fd7df65ae989d2704cbb138e905b94f245deef85
jhooper9 <j.hooper@gatech.edu>
2020-07-17 08:38:48 -0400

    New merge with premaster for pull request

--- force-app/main/default/aura/QuestionCreation/QuestionCreationHelper.js
+++ force-app/main/default/aura/QuestionCreation/QuestionCreationHelper.js
@@ -1,6 +1,6 @@
 ({
     //Sets the meetings attribute with a list of all Meeting records obtained from the apex controller.
-    loadMeetings : function(component, event) {
+    loadMeetingsAndTypeMap : function(component, event) {
         var getMeetingsAction = component.get('c.loadAllMeetings');
         getMeetingsAction.setCallback(this, function(response){
 
@@ -9,12 +9,22 @@
             }
         })
         $A.enqueueAction(getMeetingsAction);
+        var getTypeMapAction = component.get("c.getTypePicklistValues");
+        getTypeMapAction.setCallback(this,function(response){
+            if(response.getState()==="SUCCESS") {
+                let bigMap = response.getReturnValue();
+                component.set("v.allStacks",Object.keys(bigMap));
+                component.set("v.typeMap",bigMap);
+            }
+        });
+        $A.enqueueAction(getTypeMapAction);
     },
     
-    saveQuestionHelper : function(component, sub, quest, meet) {
+    saveQuestionHelper : function(component, sub, quest, type, stack, meet, associate) {
         //Initialize the apex controller and set it's paramaters
         var saveQuestionAction = component.get("c.saveQuestion");
-        saveQuestionAction.setParams({subject : sub, question : quest, meeting: meet});
+        saveQuestionAction.setParams({subject : sub, question : quest, meeting: meet,
+                                      type : type, stack : stack, associate: associate});
   
         //Set the callback and enqueue the apex controller
         saveQuestionAction.setCallback(this, function(response){
@@ -22,10 +32,18 @@
                 //After the record is submitted, reset the component values to blank
                 component.set("v.subject", null);
                 component.set("v.question", null);
+            	alert("You have successfuly entered a question");
             }
             //Display message to user of a successful transaction
-            alert("You have successfuly entered a question");
         })
         $A.enqueueAction(saveQuestionAction); 
+    },
+    
+    getUserType : function(component) {
+        let action = component.get('c.isAssociate');
+        action.setCallback(this, (res) => {
+            component.set('v.isAssociate', res.getReturnValue());
+        });
+        $A.enqueueAction(action);
     }
 })
