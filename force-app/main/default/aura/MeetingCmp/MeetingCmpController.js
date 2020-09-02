b13798c1835cffa92f9d9b987d634166e65f208e
harrysanderson98 <harrysandy1998@gmail.com>
2020-09-02 12:24:57 -0400

    Added Record types, Added 'Not Scheduled' value to Meeting Status picklist on Meeting__c

--- force-app/main/default/aura/MeetingCmp/MeetingCmpController.js
+++ force-app/main/default/aura/MeetingCmp/MeetingCmpController.js
@@ -2,14 +2,22 @@
     // Makes callout to apex controller 'MeetingController' to get boolean return value
 	doInit : function(component, event, helper) {
         helper.getMeetingId(component, event);
+        let timeslotEvent = component.get("CreateTimeSlotEvent");
+        timeslotEvent.fire();
 		let getPerm = component.get("c.getProfilePerm");  
         getPerm.setCallback(this, function(response){
             if(response.getState() === "SUCCESS") {
-            component.set("v.AssociatePerm", response.getReturnValue());
+            component.set("v.AdminPerm", response.getReturnValue());
             }
             });
         
        $A.enqueueAction(getPerm); 
-	}
+	},
+    
+    confirmTrue : function(component){
+        let confTrue = true;
+        component.set("v.confirmIsTrue", confTrue);
+        
+    }
     
 })
