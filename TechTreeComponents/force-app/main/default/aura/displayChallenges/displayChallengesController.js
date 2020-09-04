5194357fe5990fde1398f490540c04d3795e601e
Jonathan Barreto <jonBarreto43@yahoo.com>
2020-09-04 14:54:48 -0400

    takeOut

--- TechTreeComponents/force-app/main/default/aura/displayChallenges/displayChallengesController.js
+++
@@ -1,6 +0,0 @@
-({
-    myAction : function(component, event, helper) {
-        var chalId = component.get("v.ChallengeApexData.Id");
-        helper.challengeCompleteButton(component, chalId);
-    }
-})
