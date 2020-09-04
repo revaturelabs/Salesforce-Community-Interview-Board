5aee0e6cd7fa1701ad9e2ddc7427fdcf0e987588
Jonathan Barreto <jonBarreto43@yahoo.com>
2020-09-04 14:43:36 -0400

    fullManifest

---
+++ force-app/main/default/aura/primaryTechButton/primaryTechButtonController.js
@@ -0,0 +1,6 @@
+({
+    getSubTech : function(component, event, helper) {
+        var primeId = component.get("v.techPrime.Id");
+        helper.subHelp(component, primeId);
+    }
+})
---
+++ force-app/main/default/aura/techDisplay/techDisplayHelper.js
@@ -0,0 +1,58 @@
+({
+    prime : function(component) {
+        console.log("here");
+        let action = component.get("c.getTech");
+        console.log("after");
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            console.log(response.getState());
+            if(state === "SUCCESS"){
+                component.set("v.primeTech", response.getReturnValue());
+                console.log(response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    setSub : function(component, event) {
+        let subTech = event.getParam("sub");
+        component.set("v.subTech", subTech);
+        component.set("v.clickedTwo", true);
+        component.set("v.clicked", false);
+    },
+
+    setCont : function(component){
+        try {
+            var items = component.get("v.ChallengeItems");
+            var newItems = [];
+            console.log(items.length);
+            console.log(items[0].Completed__c);
+            for(let i = 0; i < items.length; i++){
+                if(items[i].Completed__c==false){
+                    newItems.push(items[i]);
+                }
+            }
+            component.set("v.ChallengeDisplay", newItems);
+        } catch(err) {
+            component.set("v.ErrBool", true);
+        }
+
+    },
+    setComp : function(component){
+        try {
+            var items = component.get("v.ChallengeItems");
+            var newItems = [];
+            console.log(items.length);
+            console.log(items[0].Completed__c);
+            for(let i = 0; i < items.length; i++){
+                if(items[i].Completed__c==true){
+                    newItems.push(items[i]);
+                }
+            }
+            component.set("v.ChallengeDisplay", newItems);
+        } catch (Err) {
+            component.set("v.ErrBool", true);
+        }
+
+    }
+})
