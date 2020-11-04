67ec07ee5b251054e05b228a89c2c69c775cd19d
MattressKnight <matthew.coger0192@gmail.com>
2020-11-04 15:53:25 -0500

    Added Javascript controller methods

--- force-app/main/default/aura/ComparePanelAnswers/ComparePanelAnswersController.js
+++ force-app/main/default/aura/ComparePanelAnswers/ComparePanelAnswersController.js
@@ -2,6 +2,7 @@
     handleCompareEvent : function(component, event, helper) {
         let communityBestAnswer = component.get('v.communityBestAnswer');
         let userAnswer = component.get('v.userAnswer');
-        helper.handleCompareEvent(component, event, communityBestAnswer, userAnswer);
+        let questionId = component.get('v.id');
+        helper.handleCompareEvent(component, event, communityBestAnswer, userAnswer, questionId);
     }
 })
--- force-app/main/default/aura/ComparePanelAnswers/ComparePanelAnswersHelper.js
+++ force-app/main/default/aura/ComparePanelAnswers/ComparePanelAnswersHelper.js
@@ -1,5 +1,17 @@
 ({
-    handleCompareEvent : function(component, event, communityBestAnswer, userAnswer) {
+    handleCompareEvent : function(component, event, communityBestAnswer, userAnswer, questionId){
+        let getBestAnswer = component.get("c.getBestResponse");
+        getBestAnswer.setParam("questionId" , questionId);
+        getBestAnswer.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS"){
+                component.set("communityBestAnswer", response.getReturnValue());
+                this.getSimilarityScore(component, event, communityBestAnswer, userAnswer);
+            }
+        });
+        $A.enqueueAction(getBestAnswer);
+    },
+
+    getSimilarityScore : function(component, event, communityBestAnswer, userAnswer) {
         let getScore = component.get("c.textSimilarity");
         getScore.setParams({"mockInterviewResponse" : userAnswer, "bestCommunityAnswer" : communityBestAnswer});
         getScore.setCallback(this, function(response){
