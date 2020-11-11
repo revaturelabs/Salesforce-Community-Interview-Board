a955f7b1031213816afcf458fbadb2247266b078
qwilliamsv <qwilliamsv@gmail.com>
2020-11-11 14:12:29 -0500

    Commented the CommunityHeader component

--- force-app/main/default/aura/CommunityHeader/CommunityHeaderController.js
+++ force-app/main/default/aura/CommunityHeader/CommunityHeaderController.js
@@ -1,6 +1,10 @@
 ({
 	init : function(component, event, helper) {
-        console.log("Run");
-        let userId = $A.get("$SObjectType.CurrentUser.Id");
+        //I originally started to write this to get the user's name, 
+        //but realized there's an easier way all on the page, 
+        //so this does nothing. 
+        //Leaving it in case someone wants to expand it in the future.
+        /*console.log("Run");
+        let userId = $A.get("$SObjectType.CurrentUser.Id");*/
 	}
 })
