93807a0a3e29ef685da598e1180a38dae35c6203
W-Hatto <chhattowil@gmail.com>
2021-03-24 11:07:02 -0500

    Congratulations Board now only displays Associates selected since the previous Monday. (More testing needed; test class for controller is incomplete.) Congratulations Board now displays Date Selected. Added Congratulations Board to InterviewForceComponent.

--- force-app/main/default/aura/CommunityBoard/CommunityBoardController.js
+++
@@ -1,9 +0,0 @@
-({
-	doInit : function(component, event, helper) {
-        let associateList = helper.loadAssociateList(component, event)
-        component.set("v.columns", [
-            					{label : 'Associate Name', fieldName : 'Name', type : 'String'},
-                       			{label: 'Client Name', fieldName : 'Client__c', type : 'String'}
-        ]);
-	}
-})
--- force-app/main/default/aura/CongratulationsBoard/CongratulationsBoardController.js
+++ force-app/main/default/aura/CongratulationsBoard/CongratulationsBoardController.js
@@ -4,7 +4,8 @@
         console.log('Associate List: ' + component.get("v.associateList"))
         component.set("v.columns", [
             					{label : 'Associate Name', fieldName : 'associateName', type : 'String'},
-                       			{label: 'Client Name', fieldName : 'clientName', type : 'String'}
+                       			{label: 'Client Name', fieldName : 'clientName', type : 'String'},
+            					{label: 'Date Selected', fieldName: 'dateSelected', type : 'Date'}
         ]);
         console.log('Columns set.')
 	}
