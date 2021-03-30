5a4cab26491e152204de8eb2c447dbb87d3f3d06
W-Hatto <chhattowil@gmail.com>
2021-03-29 19:21:00 -0500

    Removed redundant security from CongratulationsBoardController and CongratulationsBoardContainer.

--- force-app/main/default/aura/CongratulationsBoardContainer/CongratulationsBoardContainerController.js
+++ force-app/main/default/aura/CongratulationsBoardContainer/CongratulationsBoardContainerController.js
@@ -1,9 +1,4 @@
-({
-    doInit : function(component, event, helper) {
-    	let isAdmin = component.get("c.isAdmin")   
-        component.set("v.isAdmin", isAdmin)
-    },
-    
+({   
 	refreshCongratsBoard : function(component, event, helper) {
 		helper.loadAssociateList(component)
 	}
